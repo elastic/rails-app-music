dd11cbe651f2224820efd46981201f5db4d19003
Emily Stolfo <emstolfo@gmail.com>
2018-09-03 16:04:16 +0200

    Define global repository objects in Rails Initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,9 +1,11 @@
 
-Elasticsearch::Persistence.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || 'localhost:9200'
+DEFAULT_CLIENT = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || 'localhost:9200'
+$artist_repository = ArtistRepository.new(client: DEFAULT_CLIENT)
+$album_repository = AlbumRepository.new(client: DEFAULT_CLIENT)
 
 if Rails.env.development?
   logger           = ActiveSupport::Logger.new(STDERR)
   logger.level     = Logger::INFO
   logger.formatter = proc { |s, d, p, m| "\e[2m#{m}\n\e[0m" }
-  Elasticsearch::Persistence.client.transport.logger = logger
+  DEFAULT_CLIENT.transport.logger = logger
 end
