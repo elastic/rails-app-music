9a45075a23079f2fb93974cb9057a61863d5b98f
Emily Stolfo <emstolfo@gmail.com>
2018-09-03 15:48:43 +0200

    Add Artist test

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -2,55 +2,37 @@ require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
 
-  setup do
-    Artist.create_index!(force: true)
-  end
-
   test "Artist #new" do
-    artist = Artist.new(name: 'Common', profile: 'etc etc', members: ['dave', 'diana'])
+    artist = Artist.new(name: 'Common', profile: 'etc etc', members: ['Jack', 'Larry'])
     assert artist.name == 'Common'
     assert artist.profile == 'etc etc'
-    assert artist.members == ['dave', 'diana']
+    assert artist.members == ['Jack', 'Larry']
   end
 
-  test "Artist #mappings" do
-    expected_mappings = {:_doc=>
-                             {:properties=>
-                                  {:created_at=>{:type=>"date"},
-                                   :updated_at=>{:type=>"date"},
-                                   :name=>{:type=>"text", :fields=>{:name=>{:type=>"text", :analyzer=>"snowball"}, :raw=>{:type=>"keyword"}}},
-                                   :profile=>{:type=>"text"},
-                                   :members=>{:type=>"text", :fields=>{:name=>{:type=>"text", :analyzer=>"snowball"}, :raw=>{:type=>"keyword"}}},
-                                   :artist_suggest=>{:type=>"object", :properties=>{:name=>{:type=>"completion"}, :members=>{:type=>"completion"}}}}}}
+  test "Artist #attributes" do
+    artist = Artist.new(name: 'Common', profile: 'etc etc', members: ['Jack', 'Larry'])
+    assert artist.attributes[:name] == 'Common'
+    assert artist.attributes[:profile] == 'etc etc'
+    assert artist.attributes[:members] == ['Jack', 'Larry']
+  end
 
-    assert expected_mappings[:_doc][:properties].keys.to_set ==
-               Artist.mappings.to_hash[:_doc][:properties].keys.to_set
-    assert expected_mappings[:_doc][:properties].all? do |key|
-      Artist.mappings.to_hash[:_doc][:properties][key] == expected_mappings[:_doc][:properties][key]
-    end
+  test "Artist mutability" do
+    artist = Artist.new(name: 'Common', profile: 'etc etc')
+    artist.name = 'FKA Twigs'
+    assert artist.name == 'FKA Twigs'
+    assert artist.profile == 'etc etc'
   end
 
   test "Artist #to_hash" do
-    artist = Artist.new(name: 'Common', profile: 'etc etc', members: ['dave', 'diana'])
-    hash_representation = artist.to_hash
-    assert hash_representation[:artist_suggest][:members] == { input: ['dave', 'diana'] }
-    assert hash_representation[:artist_suggest][:name] == { input: [ 'Common' ]}
-  end
-
-  test "Artist#all" do
-    Artist.create({ name: 'Common' }, refresh: true)
-    Artist.create({ name: 'Common' }, refresh: true)
-    assert Artist.all.collect(&:name) == ['Common', 'Common']
+    artist = Artist.new(name: 'Common', profile: 'etc etc', members: ['Jack', 'Larry'])
+    hash = artist.to_hash
+    assert hash[:name] == 'Common'
+    assert hash[:profile] == 'etc etc'
+    assert hash[:members] == ['Jack', 'Larry']
   end
 
   test "Artist validation" do
-    artist = Artist.new(profile: 'etc etc', members: ['dave', 'diana'])
-    assert_not artist.save
-  end
-
-  test "Artist#albums" do
-    artist = Artist.create(name: 'Common', profile: 'etc etc', members: ['dave', 'diana'])
-    Album.create({ title: 'Like Water for Chocolate', artist: artist }, refresh: true)
-    assert artist.albums.first.title == 'Like Water for Chocolate'
+    artist = Artist.new(profile: 'etc etc', members: ['Jack', 'Larry'])
+    assert_not artist.valid?
   end
 end
